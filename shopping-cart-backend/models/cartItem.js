d14bfcfd5067aa903dd71d3c028137f0a153e4ff
Shubham Tiwari <meshubhamtiwarji@gmail.com>
2025-04-20 12:42:50 +0530

    Product and cart mongoDB models integrated

---
+++ shopping-cart-backend/models/cartItem.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const cartItemSchema = new mongoose.Schema({
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  quantity: { type: Number, required: true, min: 1 },
+},{ collection : 'CartItem'});
+
+module.exports = mongoose.model('CartItem', cartItemSchema);
